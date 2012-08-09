f18b5a5378dfdc6413806aeb77bda22517cb6e7e
Matt Leotta <matt.leotta@kitware.com>
2012-08-09 15:38:07 -0400

    link directories no longer needed

--- Useviscl.cmake
+++ Useviscl.cmake
@@ -19,9 +19,6 @@ ENDIF(viscl_BUILD_SETTINGS_FILE)
 # Use the standard viscl include directories.
 INCLUDE_DIRECTORIES(${viscl_INCLUDE_DIR})
 
-# Add link directories needed to use viscl.
-LINK_DIRECTORIES(${viscl_LIBRARY_DIR}/libraries)
-
 # include the vistk macros
 include(${viscl_DIR}/viscl-macros.cmake)
 
