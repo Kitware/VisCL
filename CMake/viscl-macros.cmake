4075e1c69a7e40696b814863817d255c99a3998a
Matt Leotta <matt.leotta@kitware.com>
2012-08-09 13:27:06 -0400

    split viscl macros from FindOpenCL

---
+++ CMake/viscl-macros.cmake
@@ -0,0 +1,18 @@
+
+set(OPENCL_STRINGIFY ${CMAKE_CURRENT_LIST_DIR}/viscl-stringify.cmake)
+if (NOT EXISTS ${OPENCL_STRINGIFY})
+  message(FATAL_ERROR "stringify.cmake not found in ${CMAKE_CURRENT_LIST_DIR}")
+endif()
+
+
+function(encode_opencl_sources cxx_file)
+  add_custom_command(OUTPUT ${cxx_file}
+    COMMAND ${CMAKE_COMMAND}
+      -D SRC_PATH:FILEPATH=${CMAKE_CURRENT_SOURCE_DIR}
+      -D CL_FILES:STRING="${ARGN}"
+      -D CXX_FILE:FILEPATH=${cxx_file}
+      -P ${OPENCL_STRINGIFY}
+    DEPENDS ${ARGN}
+      ${OPENCL_STRINGIFY}
+  )
+endfunction()
